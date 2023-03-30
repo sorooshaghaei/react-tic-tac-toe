8d1ac3063dc6ff6d826f13c721ebab79a1718ba3
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-03-31 01:35:33 +0330

    the game is ready, but we have more to do...

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import "../Board-row.css";
-import "../square.css";
+import "../style.css";
 
 const Square = ({ value, setValue }) => {
   return (
@@ -15,9 +14,10 @@ const Board = () => {
   const [square, setSquare] = useState(Array(9).fill(null));
 
   const clickHandler = (i) => {
-    if (square[i]) {
+    if (square[i] || CalculateWinner(square)) {
       return;
     }
+
     const nextSquare = square.slice();
     if (xIsNext) {
       nextSquare[i] = "X";
@@ -29,9 +29,20 @@ const Board = () => {
     setXIsNext(!xIsNext);
   };
 
+  const winner = CalculateWinner(square);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else if (square.every((i) => i !== null) && !winner) {
+    status = "Tie game!";
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
       <div className="Board-size">
+        <div className="status">{status}</div>
         <div className="Board-row">
           <Square value={square[0]} setValue={() => clickHandler(0)} />
           <Square value={square[1]} setValue={() => clickHandler(1)} />
@@ -50,6 +61,26 @@ const Board = () => {
       </div>
     </>
   );
+};
+
+const CalculateWinner = (square) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+      return square[a];
+    }
+  }
+  return null;
 };
 
 export default Board;
